4a9a61bbcffd820fa2681b3a8a095b4410edafff
daoraCode <severin.mboukou@gmail.com>
2021-11-16 16:38:22 +0100

    PopularBattle file updated

--- src/pages/PopularBattle.js
+++ src/pages/PopularBattle.js
@@ -5,12 +5,13 @@ import Card from '../components/Card';
 class PopularBattle extends Component {
 
 
-    constructor() {
+    constructor () {
         super()
 
         this.state = {
             movies: [],
-            currentBattle: 0
+            currentBattle: 0,
+            favorites: []
         }
 
         // this.handleMovieCardClick = this.handleMovieCardClick(bind);
@@ -25,31 +26,44 @@ class PopularBattle extends Component {
         //result.results = we only want some information within results key
     }
 
-    handleMovieCardClick() {
-        this.setState({ movies: []})
+    // We need to use an id parameter to retrieve each movie (each card index)
+    // 
+    handleMovieCardClick(id) {
+        let { currentBattle, favorites } = this.state;
+        if (currentBattle === this.state.movies.length - 2) {
+            alert("Vous avez parcouru tous les films");
+        } else {
+            this.setState({
+                currentBattle: currentBattle += 2,
+                favorites: [...favorites, id]
+            })
+
+        }
+        console.log(id);
     }
 
     render() {
 
         // data state defined to reuse them shortly
-        const { movies } = this.state;
-        const filteredMovies = movies.filter((movie, index) => {
-            return index < 2
-        });
+        const { movies, favorites } = this.state;
+        const filteredMovies = movies.filter((movie, index) => { return index === this.state.currentBattle || index === this.state.currentBattle + 1 });
+
+        localStorage.setItem("favorite", JSON.stringify(favorites)); // localStorage browser data store
 
         return (
             <>
                 <div className="container-fluid mx-5 my-5 text-center">
                     <div className="row">
-                    {filteredMovies.map(movie => (
-                         <Card
-                         key={movie.id}
-                         poster={movie.poster_path}
-                         title={movie.title}
-                         release={movie.release_date}
-                         description={movie.description}
-                     />
-                    ))}
+                        {filteredMovies.map(movie => (
+                            <div className="col-6 col-ms-6" style={{ width: "450px" }} onClick={() => this.handleMovieCardClick(movie.id)}>
+                                <div className="movieBox">
+                                    <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt="Poster Movie" />
+                                    <h1>{movie.title}</h1>
+                                    <h1>{movie.release}</h1>
+                                    <p>{movie.overview}</p>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </>
